b2d23ebf3c96571cc7ccee5d6dd948f02f7aeb67
Laparre Quentin <quentin.laparre@gmail.com>
2024-06-11 14:31:23 +0200

    Ajout de la class Jeu

--- src/metier/Jeu.java
+++ src/metier/Jeu.java
@@ -1,10 +1,47 @@
 package metier;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Jeu {
 
+	private List<Image> lstImage = new ArrayList<Image>();
+	private boolean finJeu;
+	private int numTour;
+	private List<Sommet> lstSommet = new ArrayList<Sommet>();
+
+	public Jeu()
+	{
+	}
 
 	public void lireTheme ()
 	{
 
+	}
+
+	public void lancerJeu()
+	{
+
+		this.finJeu = false;
+		this.numTour = 0;
+
+	}
+
+	public boolean estFinJeu()
+	{
+		for (Sommet s: this.lstSommet)
+		{
+			if (s.getProprietaire() == null)
+			{
+				this.finJeu = true;
+			}
+		}
+
+		return this.finJeu;
+	}
+
+	public boolean prendreMine()
+	{
+		
 	}
 }
