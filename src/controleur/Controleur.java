ec73d1349ea00d80775b57a98ca4003231e7fb3f
Evan Cnaepelnickx <evan.cnaepelnickx@etu.univ-lehavre.fr>
2024-06-11 13:59:24 +0200

    Modification controleur.java et compile.list

--- src/controleur/Controleur.java
+++ src/controleur/Controleur.java
@@ -1,8 +1,27 @@
 package controleur;
 
+import metier.*;
+import ihm.*;
+
 public class Controleur
 {
-	public static void main(String[] args) {
+	private Jeu jeu;
+
+	private FramePlateau framePlateau;
+	private FrameJoueur frameJoueur;
+
+
+	public Controleur()
+	{
+		this.jeu = new Jeu();
+
+		this.framePlateau = new FramePlateau(this);
+		this.frameJoueur = new FrameJoueur(this);
+	}
+
+	public static void main(String[] args)
+	{
+		Controleur ctrl = new Controleur();
 		System.out.println("Hello World");
 	}
 }
