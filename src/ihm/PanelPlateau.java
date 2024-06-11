0486c717268589d7eb2554b267f94bd5dfa0c247
Evan Cnaepelnickx <evan.cnaepelnickx@etu.univ-lehavre.fr>
2024-06-11 10:32:27 +0200

    Base ihm

--- src/ihm/PanelPlateau.java
+++ src/ihm/PanelPlateau.java
@@ -1,5 +1,17 @@
 package ihm;
 
-public class PanelPlateau {
-	
+import controleur.Controleur;
+
+import javax.swing.*;
+
+public class PanelPlateau extends JPanel
+{
+	public PanelPlateau(Controleur ctrl)
+	{
+		/* Création des composants */
+		
+		/* Positionnement des composants */
+		
+		/* Activation des composants */
+	}
 }
