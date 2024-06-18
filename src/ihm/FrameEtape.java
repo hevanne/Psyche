779f349b2a836c735fc8be5bf95872feebe2c492
RafaelD135 <debeinrafael@gmail.com>
2024-06-18 09:10:56 +0200

    Création de FrameEtape et PanelEtape

--- src/ihm/FrameEtape.java
+++ src/ihm/FrameEtape.java
@@ -7,9 +7,18 @@ import controleur.Controleur;
 public class FrameEtape extends JFrame
 {
 	private Controleur ctrl;
+	private PanelEtape panelEtape;
 
 	public FrameEtape(Controleur ctrl)
 	{
 		this.ctrl = ctrl;
+		this.panelEtape = new PanelEtape(this.ctrl);
+		this.add(this.panelEtape);
+		this.setVisible(true);
+		this.pack();
+	}
+
+	public static void main(String[] args) {
+		new FrameEtape(new Controleur());
 	}
 }
