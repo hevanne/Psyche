ad8dcf4a3252180bffc930df190d7dce3c90625a
Evan Cnaepelnickx <evan.cnaepelnickx@etu.univ-lehavre.fr>
2024-06-18 10:53:19 +0200

    Amelioration scripts

--- run.sh
+++ run.sh
@@ -12,8 +12,15 @@ echo "Choisissez votre scenario : (entrez le numero correspondant au scenario)"
 echo "1. Scenario 1"
 echo "2. Scenario 2"
 echo "3. Scenario 3"
+echo "4. Jeu sans scenario"
 
 read -p "Choix : " scenario
+
+while [ $scenario -ne 1 ] && [ $scenario -ne 2 ] && [ $scenario -ne 3 ] && [ $scenario -ne 4 ]
+do
+	echo "Choix invalide"
+	read -p "Choix : " scenario
+done
 
 if [ $scenario -eq 1 ]
 then
@@ -24,6 +31,9 @@ then
 elif [ $scenario -eq 3 ]
 then
 	java Psyche scenario3
+elif [ $scenario -eq 4 ]
+then
+	java Psyche
 else
 	echo "Choix invalide"
 fi
