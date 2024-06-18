ab3f369f2778e62c8ab5136174b8af6d41383fbd
Evan Cnaepelnickx <evan.cnaepelnickx@etu.univ-lehavre.fr>
2024-06-18 09:58:06 +0200

    Modification des fichiers run pour lancement des scénarios

--- run.bat
+++ run.bat
@@ -8,6 +8,21 @@ javac @compile.list -d ../bin
 REM Exécution
 cd ../bin
 
-java Psyche
+echo Choisissez un scenario :
+echo 1 - Scenario 1
+echo 2 - Scenario 2
+echo 3 - Scenario 3
+
+set /p scenario=Choix :
+
+if %scenario%==1 (
+	java Psyche scenario1
+)
+
+if %scenario%==2 (
+	java Psyche scenario2
+)
+
+if %scenario%==3 (
 
 pause
